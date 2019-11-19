d1db28ce21bf269089406aa81061217b2060a445
Malte Fries <malte.fries@draeger.com>
2019-11-19 10:33:36 +0100

    Eingebettete Resource

--- Microsoft.Dynamics365.UIAutomation.Browser/Properties/Resources.Designer.cs
+++ Microsoft.Dynamics365.UIAutomation.Browser/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace Microsoft.Dynamics365.UIAutomation.Browser.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace Microsoft.Dynamics365.UIAutomation.Browser.Properties {
         ///   Gibt die zwischengespeicherte ResourceManager-Instanz zurück, die von dieser Klasse verwendet wird.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.Dynamics365.UIAutomation.Browser.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace Microsoft.Dynamics365.UIAutomation.Browser.Properties {
         ///   Ressourcenzuordnungen, die diese stark typisierte Ressourcenklasse verwenden.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace Microsoft.Dynamics365.UIAutomation.Browser.Properties {
         /// <summary>
         ///   Sucht eine lokalisierte Ressource vom Typ System.Byte[].
         /// </summary>
-        internal static byte[] AutoAuthProxy {
+        public static byte[] AutoAuthProxy {
             get {
                 object obj = ResourceManager.GetObject("AutoAuthProxy", resourceCulture);
                 return ((byte[])(obj));
@@ -86,7 +86,7 @@ namespace Microsoft.Dynamics365.UIAutomation.Browser.Properties {
         ///            document.addEventListener(&apos;keypress&apos;, Recorder.keyHandler, false);
         ///          [Rest der Zeichenfolge wurde abgeschnitten]&quot;; ähnelt.
         /// </summary>
-        internal static string Recorder {
+        public static string Recorder {
             get {
                 return ResourceManager.GetString("Recorder", resourceCulture);
             }
