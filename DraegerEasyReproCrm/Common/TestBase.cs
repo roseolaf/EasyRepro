bc720a63aea08e5547637f148a1c0c3f78f2444a
Malte Fries <malte.fries@draeger.com>
2020-01-10 12:44:25 +0100

    Readded ADOManager

--- DraegerEasyReproCrm/Common/TestBase.cs
+++ DraegerEasyReproCrm/Common/TestBase.cs
@@ -153,7 +153,7 @@ namespace Draeger.Dynamics365.Testautomation.Common
 
 
                 Logger.TestResult("{TestResult}", TestContext.CurrentTestOutcome);
-#if DEBUG
+#if !DEBUG
                 if (Exception != null)
                 {
                     if (Exception.Message.Contains("Assert"))
